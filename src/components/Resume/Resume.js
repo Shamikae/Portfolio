793d504c74aeff400b0458820246fe52c45062c0
Shamika Earle <e.shamika@yahoo.com>
2021-06-10 13:05:41 -0400

    test update

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './Resume.css';
-import resume from '../../Assets/SoftEnginResume.pdf';
+import resume from '../../Assets/SoftEnginResume.png';
 
 class Resume extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {};
     }
     render() {
         return (
-            <div className='resume' name='Resume'><h1 className="sectionHead">Resume</h1>
-            <embed src={resume}/>
+            <div className='resume' name='Resume'>
+                <h1 className="sectionHead">Resume</h1>
+                <img className='resumeImg' src={resume} />
             </div>
         )
     }
